4d493d6100f8287cac92d14461f8ad2701b29705
Florian Jeunet <aelegriniv@gmail.com>
2023-10-07 12:28:03 +0200

    changement mot_complet pour éviter les pbs de typage

--- fonctions.py
+++ fonctions.py
@@ -12,28 +12,31 @@ def choix_du_mot():
     i = random.randint(0,len(mots_possibles)-1)
 
     mot_a_trouver=mots_possibles[i]
+    mot_a_trouver=mot_a_trouver[:-1]
     return mot_a_trouver
 
-def mot_complet(mot):
-    mot_a_trouver = choix_du_mot
+def mot_complet(mot,mot_a_trouver):
+       
     if len(mot) != len(mot_a_trouver) or mot != mot_a_trouver:
         return False
     else:
         return True
     
 def lettre(caractere,liste_temporaire,mot_a_trouver):
-    k = 0
     
+    k = 0
     if caractere in liste_temporaire:
+        return k
+    
+    k+=1
 
     for i in range(len(mot_a_trouver)):
         if caractere == mot_a_trouver[i]:
             liste_temporaire[i] = caractere
             k+=1
-    if k == 0:
-        return False
-    else: 
-        return True
+
+    return k
+    
 
 
 
